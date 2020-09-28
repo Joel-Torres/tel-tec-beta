29f23557998f7606ba97806a6c510d4ff43f73d7
joel <joel.torres.dev@gmail.com>
2020-09-28 14:55:24 -0700

    save img and pages

--- callus.js
+++ callus.js
@@ -4,7 +4,7 @@ var app = new Reef('#callus');
 // Your template can be a string
 var app = new Reef('#callus', {
 	template: '<h4><small>Call us for a free needs analysis.</small></h4>' +
-				'<h3>Who is protecting you?</h3>' + 
+				'<h3>We Make Security Easy</h3>' + 
 				'<div class="text-center">' +
 				'<h2>Tel-Tec Security Systems</h2>' +
 				'<p>Protecting people and their assets since 1982</p>' +
